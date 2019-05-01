b3dbc9d9d090d92238dedf73eab0c2e4a417922a
DESKTOP-PI0J4Q2\Gamma Test <web.crawlers1000@gmail.com>
2019-05-01 17:23:23 +0300

    First commit to repository

---
+++ ClassLibrary1/catena_test_suite.cs
@@ -0,0 +1,143 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Diagnostics;
+using System.Net;
+using System.IO;
+using System;
+
+
+namespace CatenaDB
+{
+    public class CreateSignature
+    {        
+        [SetUp]
+
+        [Test, Description ("Create signature")]
+
+        public async System.Threading.Tasks.Task CreateSimpleSignature(HttpClient client)
+        {
+
+                //var client = new HttpClient();
+                var postData = new List<KeyValuePair<string, string>>();
+                postData.Add(new KeyValuePair<string, string>("Name", "Grace Hopper"));
+                postData.Add(new KeyValuePair<string, string>("request", "{\"dataHash\": {\"algorithm\": \"SHA-256\",\"value\": \"GHgonhozPdhQkdMPABufb57U1CjVfgSb0OvU28ib0hA=\"  },  \"level\": 0,  \"metadata\": { }        }"));
+
+                var formContent = new FormUrlEncodedContent(postData);
+                string requestContent = await formContent.ReadAsStringAsync();
+                var response = await client.PostAsync("https://tryout-catena-db.guardtime.net/api/v1/signatures", formContent);
+                var content = await response.Content.ReadAsStringAsync();
+                Assert.That(response.IsSuccessStatusCode);
+                Debug.WriteLine(message: "Request:" + requestContent);
+                Debug.WriteLine(message: "Response:" + content.ToString());
+
+        }
+        [Test, Description("Creating the new asynsronous signature")]
+        public async System.Threading.Tasks.Task CreateAsynchronousSignature()
+        {
+
+            var client = new HttpClient();
+            var postData = new List<KeyValuePair<string, string>>();
+            postData.Add(new KeyValuePair<string, string>("Name", "Grace Hopper"));
+            postData.Add(new KeyValuePair<string, string>("request", "{\"dataHash\": {\"algorithm\": \"SHA-256\",\"value\": \"GHgonhozPdhQkdMPABufb57U1CjVfgSb0OvU28ib0hA=\"  },  \"level\": 0,  \"metadata\": { }        }"));
+
+            var formContent = new FormUrlEncodedContent(postData);
+            string requestContent = await formContent.ReadAsStringAsync();
+            var response = await client.PostAsync("https://tryout-catena-db.guardtime.net/api/v1/signatures", formContent);
+            var content = await response.Content.ReadAsStringAsync();
+            Assert.That(response.IsSuccessStatusCode);
+            Debug.WriteLine(message: "Request:" + requestContent);
+            Debug.WriteLine(message: "Response:" + content.ToString());
+
+        }
+
+
+    }
+    public class ReadSignature
+    {
+        [SetUp]
+        public void SetupTest()
+        {
+
+        }
+        [Test, Description("Create signature")]
+
+        public void GetExistingSignatureDetails()
+        {
+            WebClient client = new WebClient();
+            client.Headers.Add("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;)");
+            string html = string.Empty;
+            string url = "https://tryout-catena-db.guardtime.net/api/v1/signatures?ids=e3f33734-09e5-4d32-b48c-658f2ade6336";
+
+            Stream data = client.OpenRead(url);
+            StreamReader reader = new StreamReader(data);
+            string s = reader.ReadToEnd();
+            data.Close();
+            reader.Close();
+            Debug.WriteLine(message: "Response:" + s.ToString());
+        }
+
+        public void GetNotExistingSignatureDetails()
+        {
+            WebClient client = new WebClient();
+            client.Headers.Add("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;)");
+            string html = string.Empty;
+            string url = "https://tryout-catena-db.guardtime.net/api/v1/signatures?ids=e3f33734-09e5-4d32-b48c-658f2ade6336";
+
+            Stream data = client.OpenRead(url);
+            StreamReader reader = new StreamReader(data);
+            string s = reader.ReadToEnd();
+            data.Close();
+            reader.Close();
+            Debug.WriteLine(message: "Response:" + s.ToString());
+            Assert.That(s.ToString().Length>0);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+
+        }
+
+        
+        }
+
+    public class ExtendSignature
+    {
+        [SetUp]
+        public void SetupTest()
+        {
+
+        }
+        [Test, Description("TC-003 Assigning unique ID to generated signature ")]
+
+        public async System.Threading.Tasks.Task ExistingSignaturPersistence()
+        {
+            var client = new HttpClient();
+            var putData = new List<KeyValuePair<string, string>>();
+            client.BaseAddress = new Uri("https://tryout-catena-db.guardtime.net/api/v1/signatures");
+            var formContent = new FormUrlEncodedContent(putData);
+            string requestContent = await formContent.ReadAsStringAsync();
+            var response = client.PutAsync("api/person", formContent).Result;
+            Assert.That(response.IsSuccessStatusCode);
+        }
+        [Test, Description("TC-004 Assigning unique ID to generated signature (V2)")]
+        public async System.Threading.Tasks.Task ExistingSignaturPersistenceV2()
+        {
+            var client = new HttpClient();
+            var putData = new List<KeyValuePair<string, string>>();
+            client.BaseAddress = new Uri("https://tryout-catena-db.guardtime.net/api/v2/signatures");
+            var formContent = new FormUrlEncodedContent(putData);
+            string requestContent = await formContent.ReadAsStringAsync();
+            var response = client.PutAsync("api/person", formContent).Result;
+            Assert.That(response.IsSuccessStatusCode);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+
+        }
+
+    }
+    }
---
+++ readme.txt
@@ -0,0 +1,18 @@
+Basic test suite for testing Guardtime version 1.0
+
+Author: Maksim Kolodijev (maksim.kolodijev@mail.ee / 55581521)
+
+DESCRIPTION:
+
+Guardtime API calls tests set. 
+
+REQUIREMENTS:
+
+Visual Studio (was developed using VS 2019, but also should be compatible with VS 2015 CE)
+NUnit3 (http://nunit.org)
+
+TESTS EXECUTION:
+
+1. Open solution with Visual Studio
+2. Build solution
+3. Run application
